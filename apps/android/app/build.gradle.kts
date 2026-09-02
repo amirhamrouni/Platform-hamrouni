39da790341224ddc0d705ddb9b86a43a5453535b
amirhamrouni <amiroscrypto2022@gmail.com>
2026-09-02 20:50:00 +0200

    release(android): cut 0.9.0-rc2

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "nl.leersprong.app"
         minSdk = 23
         targetSdk = 37
-        versionCode = 9
-        versionName = "0.9.0-rc1"
+        versionCode = 10
+        versionName = "0.9.0-rc2"
     }
 
     buildFeatures {
